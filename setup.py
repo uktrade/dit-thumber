b8764be2b24f5bb57efafe2470e084c88251a54a
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-10-11 23:02:53 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
+# allow setup.py to be run from any path.
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
